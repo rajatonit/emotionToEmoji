ef876fd920381d9514f874fc240bc2ad3704b12c
Rajat Arora <rarora@loyalty.com>
2019-11-25 09:08:55 -0500

    added try and catch around fetch emotions

--- scraper/lib/fetchEmotions.js
+++ scraper/lib/fetchEmotions.js
@@ -4,7 +4,7 @@ const fs = require ('fs').promises;
 var fetchEmotions = {};
 
 fetchEmotions.fetch = async (keyword, limit) => {
-  console.log ('Fetching emotions');
+  console.log (`Fetching emotions - ${keyword}`);
   let google = new Scraper.Google ({
     keyword,
     limit,
@@ -17,8 +17,11 @@ fetchEmotions.fetch = async (keyword, limit) => {
       color: undefined, // options: color, gray, trans
     },
   });
-
-  const results = await google.start ();
+  try {
+    const results = await google.start ();
+  } catch (err) {
+    throw err;
+  }
   console.log ('Fetched Emotions');
 
   return results;
@@ -26,12 +29,9 @@ fetchEmotions.fetch = async (keyword, limit) => {
 
 fetchEmotions.save = async (data, filename) => {
   try {
-    await fs.writeFile (
-      `./dist/json/${filename}.json`,
-      JSON.stringify (data)
-    );
+    await fs.writeFile (`./dist/json/${filename}.json`, JSON.stringify (data));
   } catch (err) {
-    console.log(err)
+    console.log (err);
     throw err;
   }
 };
@@ -39,10 +39,10 @@ fetchEmotions.save = async (data, filename) => {
 fetchEmotions.read = async filename => {
   try {
     const file = await fs.readFile (`./dist/json/${filename}.json`);
-    return JSON.parse(file);
+    return JSON.parse (file);
   } catch (err) {
     throw err;
   }
 };
 
-module.exports= fetchEmotions;
+module.exports = fetchEmotions;
